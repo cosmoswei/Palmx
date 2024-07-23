c36294fb4e7c10d367b02b91971063c146d23434
宇宙伟 <2117008741@qq.com>
2024-07-24 00:04:50 +0800

    http3_v3

--- src/main/java/me/xuqu/palmx/flowcontrol/impl/SlidingWindowFlowControl.java
+++ src/main/java/me/xuqu/palmx/flowcontrol/impl/SlidingWindowFlowControl.java
@@ -5,15 +5,39 @@ import lombok.extern.slf4j.Slf4j;
 import me.xuqu.palmx.flowcontrol.AbstractFlowControl;
 import me.xuqu.palmx.flowcontrol.FlowControlReq;
 
+import java.util.LinkedList;
+import java.util.Queue;
+
 @Slf4j
 public class SlidingWindowFlowControl extends AbstractFlowControl {
 
+    private final int maxRequests;
+    private final long windowSizeInMillis;
+    private final Queue<Long> requestTimestamps;
+
     public SlidingWindowFlowControl(int qps) {
+        this.maxRequests = qps;
+        this.windowSizeInMillis = 1 * 1000;
+        this.requestTimestamps = new LinkedList<>();
         super.qps = qps;
     }
 
     @Override
     public boolean doControl(FlowControlReq flowControlReq) {
-        return false;
+        long currentTime = System.currentTimeMillis();
+        long windowStart = currentTime - windowSizeInMillis;
+        // Remove timestamps that are outside of the window
+        while (!requestTimestamps.isEmpty() && requestTimestamps.peek() < windowStart) {
+            requestTimestamps.poll();
+        }
+        if (requestTimestamps.size() < maxRequests) {
+            requestTimestamps.add(currentTime);
+            log.debug("Request allowed at {}", currentTime);
+            return true;
+        } else {
+            log.debug("Request denied at {}", currentTime);
+            return false;
+        }
     }
+
 }
--- src/main/java/me/xuqu/palmx/metric/MetricMetadata.java
+++ src/main/java/me/xuqu/palmx/metric/MetricMetadata.java
@@ -1,4 +1,43 @@
 package me.xuqu.palmx.metric;
 
 public class MetricMetadata {
+
+    /**
+     * CPU 负载 %
+     */
+    private String cpuLoad;
+
+    /**
+     * CPU使用率 %
+     */
+    private String cpuUsage;
+
+    /**
+     * 内存使用率 %
+     */
+    private String memUsage;
+
+    /**
+     * 磁盘负载 %
+     */
+    private String diskLoad;
+
+    /**
+     * 网络延迟 ms
+     */
+    private String latency;
+
+    /**
+     * 带宽使用率 %
+     */
+    private String bandwidthUtilization;
+
+    /**
+     * 并发连接数
+     */
+    private String concurrentConnections;
+    /**
+     * 丢包率 %
+     */
+    private String packetLossRate;
 }
