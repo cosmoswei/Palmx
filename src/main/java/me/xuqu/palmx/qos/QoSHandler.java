2ecaecef5f837318ef91c5445afdcb4baee9dae0
宇宙伟 <2117008741@qq.com>
2024-08-25 17:17:50 +0800

    http3_v4

--- src/main/java/me/xuqu/palmx/qos/QoSHandler.java
+++ src/main/java/me/xuqu/palmx/qos/QoSHandler.java
@@ -3,14 +3,21 @@ package me.xuqu.palmx.qos;
 import lombok.extern.slf4j.Slf4j;
 import me.xuqu.palmx.metric.MetricsHolder;
 import me.xuqu.palmx.metric.pojo.Metrics;
+import me.xuqu.palmx.rule.Metric2QoSRule;
+import me.xuqu.palmx.rule.QoS;
 
-import java.util.Random;
+import javax.annotation.Resource;
 
 @Slf4j
 public class QoSHandler {
+
+    @Resource
+    private static Metric2QoSRule metric2QoSRule;
+
     public static int getQoSLevel(Metrics metrics) {
         // 通过规则引擎给出QoS等级
-        return new Random().nextInt(100);
+        QoS qoS = metric2QoSRule.getQoS(metrics);
+        return qoS.getQoSLevel();
     }
 
     public static int getLocalQoSLevel() {
--- src/main/java/me/xuqu/palmx/rule/QoS.java
+++ src/main/java/me/xuqu/palmx/rule/QoS.java
@@ -4,5 +4,10 @@ import lombok.Data;
 
 @Data
 public class QoS {
-    private int qoSLevel;
+    /**
+     * QoS 等级，体现的是服务质量的分数
+     * 值域 = 1-100
+     * 默认值 = 100
+     */
+    private int qoSLevel = 100;
 }
